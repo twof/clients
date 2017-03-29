e0653c6cb5b08186a225f06a2151b22045f38f9e
Tanner Nelson <me@tanner.xyz>
2017-03-29 21:18:41 +0200

    beta

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .Package(url: "git@github.com:vapor-cloud/models.git", majorVersion: 0),
 
         // HTTP, WebSockets, Streams, SMTP, etc.
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
 
         // JSON Web Tokens in Swift by @siemensikkema.
         .Package(url: "https://github.com/vapor/jwt.git", majorVersion: 0)
