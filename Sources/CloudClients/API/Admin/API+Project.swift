83e9e1413e39664468a2f9f9d6c4b362940a722d
tanner0101 <me@tanner.xyz>
2017-04-25 16:52:38 +0200

    organization

--- Sources/CloudClients/API/Admin/API+Project.swift
+++ Sources/CloudClients/API/Admin/API+Project.swift
@@ -1,7 +1,15 @@
 extension CloudAPI {
     /// Index
-    public func projects() throws -> [Project] {
+    public func projects(
+        organization: ModelOrIdentifier<Organization>? = nil
+    ) throws -> [Project] {
         let req = try makeRequest(.get, path: "admin", "projects")
+        var json = JSON()
+        if let org = organization {
+            try json.set("organizationId", org.getIdentifier())
+        }
+        req.json = json
+        
         let res = try respond(to: req)
         return try [Project](
             json: res.assertJSON().get("data")
