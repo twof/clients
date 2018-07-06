9ab7f28f3ad3f1dcb848b77711e1029b12d7eda9
Jonas Schwartz <jonas@qutheory.io>
2018-07-07 01:23:46 +0200

    Adding pagination on organization

--- Sources/CloudClients/API/Admin/API+Organization.swift
+++ Sources/CloudClients/API/Admin/API+Organization.swift
@@ -1,18 +1,27 @@
 extension CloudAPI {
     /// Index
     public func organizations(
-        nameLike name: String? = nil
-    ) throws -> [Organization] {
+        nameLike name: String? = nil,
+        page: Int? = nil,
+        size: Int? = nil
+    ) throws -> Page<Organization> {
         let req = try makeRequest(.get, path: "admin", "organizations")
         var json = JSON()
+        if let page = page {
+            try json.set("page", page)
+        }
+        if let size = size {
+            try json.set("size", size)
+        }
         if let name = name {
             try json.set("name", name)
         }
         req.json = json
         
         let res = try respond(to: req)
-        return try [Organization](
-            json: res.assertJSON().get("data")
+        //return try Page<Project>(json: res.assertJSON())
+        return try Page<Organization>(
+            json: res.assertJSON()
         )
     }
     
