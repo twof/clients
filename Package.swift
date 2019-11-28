5c34f2dcbc3d8c2120e3a68c3f1869df16ffb2c2
Alex Reilly <areilly@yelp.com>
2019-11-28 10:16:15 -0800

    add target

--- Package.swift
+++ Package.swift
@@ -5,12 +5,14 @@ let package = Package(
     name: "CloudClients",
     dependencies: [
         // Vapor Cloud models.
-        .package(url: "git@github.com:vapor-cloud/models.git", from: "0.0.1"),
+        .package(url: "https://github.com/twof/models.git", .branch("PackageUpgrade")),
 
         // HTTP, WebSockets, Streams, SMTP, etc.
         .package(url: "https://github.com/vapor/vapor.git", from: "2.0.0"),
 
         // JSON Web Tokens in Swift by @siemensikkema.
         .package(url: "https://github.com/vapor/jwt.git", from: "2.0.0")
-    ] 
+    ], targets: [
+        .target(name: "CloudClients", dependencies: ["CloudModels", "Vapor", "JWT"])
+    ]
 )
