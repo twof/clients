4cf803c129c67ef98f93244876f5d31958b3dfb4
Alex Reilly <areilly@yelp.com>
2019-11-28 09:52:38 -0800

    Package.swift upgraded

--- Package.swift
+++ Package.swift
@@ -1,15 +1,16 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "CloudClients",
     dependencies: [
         // Vapor Cloud models.
-        .Package(url: "git@github.com:vapor-cloud/models.git", majorVersion: 0),
+        .package(url: "git@github.com:vapor-cloud/models.git", from: "0.0.1"),
 
         // HTTP, WebSockets, Streams, SMTP, etc.
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.0.0"),
 
         // JSON Web Tokens in Swift by @siemensikkema.
-        .Package(url: "https://github.com/vapor/jwt.git", majorVersion: 2)
+        .package(url: "https://github.com/vapor/jwt.git", from: "2.0.0")
     ] 
 )
